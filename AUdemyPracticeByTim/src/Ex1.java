ef458a552e85d76f6640a54553840eb8b2091e66
shantiox <danaciocoiu78@gmail.com>
2019-05-03 16:00:57 +0300

    ignore

---
+++ AUdemyPracticeByTim/src/Ex1.java
@@ -0,0 +1,13 @@
+public class Ex1 {
+
+    public static void main(String[] ars) {
+
+        String s = "java2s".replace('a', 'Z').trim().concat("Aa");
+        s.substring(0, 2);
+        System.out.println(s);
+
+
+    }
+                }
+
+
