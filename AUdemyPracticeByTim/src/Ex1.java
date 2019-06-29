aacfe222642d9b151a355578b7ce99bc084ad109
shantiox <danaciocoiu78@gmail.com>
2019-06-29 11:36:53 +0300

    practice

--- AUdemyPracticeByTim/src/Ex1.java
+++ AUdemyPracticeByTim/src/Ex1.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class Ex1 {
 
     public static void main(String[] ars) {
@@ -6,8 +8,32 @@ public class Ex1 {
         s.substring(0, 2);
         System.out.println(s);
 
+//Write a program that creates and prints a random phone number of the form XXX-XXX-XXXX.
+//Include the dashes in the output. Do not let the first three digits contain an 8 or 9 ,
+// the second set of three digits is no greater than 742.
+        Random rand = new Random();
+        int first = rand.nextInt(8);
+        int second = rand.nextInt(8);
+        int third = rand.nextInt(8);
+        int middle = rand.nextInt(742);
+        int last = rand.nextInt(9999);
+        String middleString;
+        String lastString;
 
+        middleString = Integer.toString(middle);
+        lastString = Integer.toString(last);
+
+        while (middleString.length() < 3) {
+            middleString = "0" + middleString;
+        }
+        while (lastString.length() < 4) {
+            lastString = "0" + lastString;
+
+        }
+        System.out.println(Integer.toString(first) + Integer.toString(second) + Integer.toString(second) + "-" + middle + "-" + last);
     }
-                }
+
+
+}
 
 
